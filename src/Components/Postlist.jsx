b58b9758e54708cb9e8650a9e9fed11f68cb3e7f
TheTyork <the.tyork1@gmail.com>
2026-08-11 16:14:19 +0300

    добвление анимации при удалении/добавлении

--- src/Components/Postlist.jsx
+++ src/Components/Postlist.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import PostItem from "./PostItem";
+import { motion, AnimatePresence } from "framer-motion";
 
 const PostList = ({ posts, title, remove }) => {
   if (!posts.length) {
     return <h3 style={{ textAlign: "center" }}>Нет постов</h3>;
   }
+
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
-      {posts.map((post) => (
-        <PostItem removeP={remove} post={post} key={post.id} />
-      ))}
+      <AnimatePresence>
+        {posts.map((post) => (
+          <motion.div
+            key={post.id}
+            // Анимация появления (при добавлении элемента)
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            // Анимация удаления (уход влево)
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.3 }}
+          >
+            <PostItem removeP={remove} post={post} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
