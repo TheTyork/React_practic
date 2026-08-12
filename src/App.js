936c6fb4ab3d9b492147c06151c9ed7e93313ec6
TheTyork <the.tyork1@gmail.com>
2026-08-12 10:29:25 +0300

    Добавление автоматического запроса сервера и анимации загрузки

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import Counter from "./Components/Counter";
 import "./styles/style.css";
 import PostItem from "./Components/PostItem";
@@ -9,58 +9,57 @@ import PostForm from "./Components/PostForm";
 import MySelect from "./Components/UI/select/MySelect";
 import PostFilter from "./Components/PostFilter";
 import MyModal from "./Components/UI/modal/MyModal";
+import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PosrService";
+import Loader from "./Components/UI/loader/Loader";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Java", body: "Java - использовалась для разработки" },
-    { id: 2, title: "Java 2", body: "Java - использовалась для разработки" },
-    { id: 3, title: "Java 3", body: "Java - использовалась для разработки" },
-    { id: 4, title: "Java 4", body: "Java - использовалась для разработки" },
-    { id: 5, title: "Java 5", body: "Java - использовалась для разработки" },
-    { id: 6, title: "Java 6", body: "Java - использовалась для разработки" },
-  ]);
-
-  // const bodyInputRef = useRef()
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
-const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setPostsLoading] = useState(false);
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
-    setModal(false)
+    setModal(false);
   };
-
-  const sortedPosts = useMemo(() => {
-    console.log("Отработало");
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort]),
-      );
-    }
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLocaleLowerCase().includes(filter.query),
-    );
-  }, [filter.query, sortedPosts]);
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  useEffect(() => {
+    fetchPosts();
+  }, [filter]);
+
+  async function fetchPosts() {
+    setPostsLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setPostsLoading(false);
+  }
+
   return (
     <div className="App">
-      <MyButton onClick = {() => setModal(true)}>Создать пост</MyButton>
+      <MyButton onClick={fetchPosts}>Заросить посты</MyButton>
+      <MyButton onClick={() => setModal(true)}>Создать пост</MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <Postlist
-        remove={removePost}
-        posts={sortedAndSearchPosts}
-        title="Список постов 1"
-      />
+      {isPostsLoading ? (
+        <div style={{display: 'flex', justifyContent: 'center'} }>
+          <Loader />
+        </div>
+      ) : (
+        <Postlist
+          remove={removePost}
+          posts={sortedAndSearchPosts}
+          title="Список постов 1"
+        />
+      )}
     </div>
   );
 }
---
+++ src/Components/UI/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import clas from './Loader.module.css';
+
+const Loader = () => {
+    return(
+        <div className={clas.loader}>
+
+        </div>
+    )
+}
+
+export default Loader
