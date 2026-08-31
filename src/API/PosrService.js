3193c4538483a5e724308797de14417ceb60968e
TheTyork <the.tyork1@gmail.com>
2026-08-31 19:56:05 +0300

    Комментарии и кнопка к списку постов

--- src/API/PosrService.js
+++ src/API/PosrService.js
@@ -21,4 +21,11 @@ export default class PostService {
     );
     return response;
   }
+
+  static async getCommentsByID(id) {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+    );
+    return response;
+  }
 }
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -3,16 +3,27 @@ import { useFetcher, useParams } from "react-router-dom";
 import { useFetching } from "../hooks/useFetching";
 import PostService from "../API/PosrService";
 import Loader from "../Components/UI/loader/Loader";
+import MyButton from "../Components/UI/btn/MyButton";
+import { useNavigate } from "react-router-dom";
 
 const PostIdPage = (props) => {
+  const router = useNavigate()
   const param = useParams();
   const [post, setPost] = useState({});
+  const [coments, setCommets] = useState([]);
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(param.id);
     setPost(response.data);
   });
+  const [fetchComment, isCommentsLoading, CommentError] = useFetching(
+    async (id) => {
+      const response = await PostService.getCommentsByID(param.id);
+      setCommets(response.data);
+    },
+  );
   useEffect(() => {
     fetchPostById(param.id);
+    fetchComment(param.id);
   }, [param.id]);
 
   return (
@@ -28,6 +39,20 @@ const PostIdPage = (props) => {
           <div>{post.body}</div>
         </div>
       )}
+      <h2> Комментарии</h2>
+      {isCommentsLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {coments.map((comm) => (
+            <div style={{ margin: 15 }}>
+              <h5>{comm.email}</h5>
+              <div>{comm.body}</div>
+            </div>
+          ))}
+        </div>
+      )}
+      <MyButton onClick = {() => {router(`/posts`)}}>Назад</MyButton>
     </div>
   );
 };
